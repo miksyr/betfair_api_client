a9037d7c539fde27ac431e480f12e4ed40b6572b
Mike Doran <mikrdoran@gmail.com>
2020-12-15 14:18:38 +0000

    version bump in setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 setuptools.setup(
     name='betfair_api_client',
     packages=setuptools.find_packages(),
-    version='0.1',
+    version='0.2',
     description='Client for connecting to the Betfair Odds API and fetch betting market info',
     author='Michael Doran',
     author_email='mikrdoran@gmail.com',
     url='https://github.com/miksyr/betfair_api_client',
-    download_url='https://github.com/miksyr/betfair_api_client/archive/v_01.tar.gz',
+    download_url='https://github.com/miksyr/betfair_api_client/archive/v_02.tar.gz',
     keywords=['betfair', 'betfair odds api', 'sports betting'],
     install_requires=['requests==2.24.0'],
     classifiers=[
